4b06a862c1bb8eb5bb696dad7d284e5f307837fb
Mauricio Lima <github@mauriciolima.eti.br>
2018-04-05 18:01:07 -0300

    Removal of the input test alert in the initialization function and application of data replacement in template with static source data in the file itself and removal of the fixed items initially used in the table

--- files/js/tasks.js
+++ files/js/tasks.js
@@ -17,13 +17,43 @@
 
     function UpdateData(data)
     {
+        data.forEach(function (item, index) {
+            var template
 
+            template = $('#task-row').html()
+            template = template.replace('{sequence}',    index + 1)
+            template = template.replace('{name}',        item.name)
+            template = template.replace('{description}', item.description)
+            template = template.replace('{start}',       item.start)
+            template = template.replace('{finish}',      item.finish)
+            template = template.replace('{status}',      item.status)
+            template = template.replace('{state}',       item.state)
+
+            $('.table tbody').append(template)
+        })
     }
 
     function getData()
     {
         return [
-
+            {
+                task_id     : 1,
+                name        : "Name 1",
+                description : "Description 1",
+                start       : "01/02/2018",
+                finish      : "01/03/2018",
+                status      : "Pendente",
+                state       : "ativo"
+            },
+            {
+                task_id     : 2,
+                name        : "Name 2",
+                description : "Description 2",
+                start       : "01/03/2018",
+                finish      : "01/04/2018",
+                status      : "Pendente",
+                state       : "ativo"
+            }
         ]
     }
  }
