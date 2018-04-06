c97d6bb12b5346bb9c3627370a6554ccda5b965d
Mauricio Lima <github@mauriciolima.eti.br>
2018-04-06 14:04:35 -0300

    Simplifying scripts and inserting test data from the tasks table

---
+++ database/tasks-data.sql
@@ -0,0 +1,6 @@
+
+/*Data for the table `tasks` */
+insert  into `tasks`(`task_id`,`name`,`description`,`start`,`finish`,`status`,`active`,`updated`) values 
+(1,'Name 1','Description 1','2018-04-05','2018-04-12',1,1,'2018-04-05 18:28:06'),
+(2,'Name 2','Description 2','2018-04-17','2018-04-27',2,1,'2018-04-05 18:28:20');
+
--- database/tasks-structure.sql
+++ database/tasks-structure.sql
@@ -1,25 +1,7 @@
-/*
-SQLyog Community v12.4.3 (64 bit)
-MySQL - 10.1.25-MariaDB : Database - tasks
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`tasks` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-
-USE `tasks`;
 
 /*Table structure for table `status` */
 
 DROP TABLE IF EXISTS `status`;
-
 CREATE TABLE `status` (
   `status_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -30,7 +12,6 @@ CREATE TABLE `status` (
 /*Table structure for table `tasks` */
 
 DROP TABLE IF EXISTS `tasks`;
-
 CREATE TABLE `tasks` (
   `task_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -43,7 +24,3 @@ CREATE TABLE `tasks` (
   PRIMARY KEY (`task_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
