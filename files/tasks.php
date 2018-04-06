b62cac5febd1eb746731ecd76f54d4530f00bbfc
Mauricio Lima <github@mauriciolima.eti.br>
2018-04-06 19:17:24 -0300

    Modifications for suport POST method in tasks PHP script

--- files/tasks.php
+++ files/tasks.php
@@ -33,7 +33,7 @@ function database_connection()
 }
 
 
-function get_data()
+function get_tasks_data()
 {
     $database = database_connection();
 
@@ -48,6 +48,102 @@ function get_data()
 }
 
 
+function insert_task_data()
+{
+    $database = database_connection();
+
+    $statment = $database->prepare('call task_insert(:name, :description, :start, :finish, :status, :active)', array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $statment->execute([':name' => 'aaaa', ':description' => 'bbbb', ':start' => '2018-01-01', ':finish' => '2018-01-01', ':status' => 1, ':active' => 1]);
+
+    header('Content-type: application/json');
+    print_r('{ "success" : true }');
+}
+
+
+function normalize_parameters($source)
+{
+  $parameters = array();
+  foreach ($source as $key => $value) 
+  { 
+    $parameters[strtolower($key)] = $value;
+  }
+  return $parameters;
+}
+
+
+function request_select()
+{
+  try
+  {
+      switch ($_SERVER['REQUEST_METHOD'])
+      {
+          case 'POST' :
+              $method = 'post';
+              if ( isset($_SERVER['CONTENT_TYPE']) )
+              {
+                switch ($_SERVER['CONTENT_TYPE'])
+                {
+                    case 'application/x-www-form-urlencoded':
+                        $parameters = normalize_parameters($_POST);
+                        break;
+
+                    case 'application/json':
+                        $a = file_get_contents('php://input');
+                        $parameters = json_decode(file_get_contents('php://input'), true);
+                        break;
+
+                    default:
+                        break;
+                }
+              }
+              break;
+
+          case 'GET'  :
+              $method = 'get';
+              $parameters = normalize_parameters($_GET);
+              break;
+
+          default :
+              http_response_code(405);
+              throw new Exception('Method \'' . $_SERVER['REQUEST_METHOD'] . '\' not supported');
+              return;        
+      }
+
+      if ( ($method == 'post') && !isset($parameters['action']) )
+      {
+          throw new Exception('Missing \'action\' in post \n' . $_SERVER['CONTENT_TYPE'] . '\n' . $a . '\n' . print_r($parameters, true));
+          return;
+      }
+
+      if ( ($method == 'get')  )
+      {
+        get_tasks_data();
+        return;
+      }
+
+      if ( ($method == 'post')  )
+      {
+        if ( $parameters['action'] != 'insert')
+        {
+            throw new Exception('Only action allowed is \'insert\'');
+            return;
+        }
+
+        insert_task_data($parameters['data']);
+        return;
+      }
+  }
+  catch (Exception $e)
+  {
+      error_handler($e->getMessage() . '[' . $e->getLine() .']');
+  }
+
+}
+
+
+request_select();
+
+?>
 get_data();
 
 ?>
