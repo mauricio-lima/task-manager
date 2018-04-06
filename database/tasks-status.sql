e5f563ff610d4a0bb95fe8d538ada6e241f9eab8
Mauricio Lima <github@mauriciolima.eti.br>
2018-04-06 14:06:51 -0300

    Inserting data from possible states into the 'status' table

---
+++ database/tasks-status.sql
@@ -0,0 +1,7 @@
+
+insert  into `status`(`status_id`,`name`,`updated`) values 
+(1,'Concluído','2018-04-05 18:34:54'),
+(2,'Pendente','2018-04-05 18:34:49'),
+(3,'Em desenvolvimento','2018-04-05 18:35:02'),
+(4,'Em teste','2018-04-05 18:35:06');
+
