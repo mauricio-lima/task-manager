5f7847d05e10b216ee799fc2423c7313af2500b5
Mauricio Lima\hp dv4 <github@mauriciolima.eti.br>
2018-04-07 22:18:52 -0300

    Modifying the stored procedure 'task_insert' to check the validity of the dates and produce output in the JSON format to be consumed by the user interface.

--- database/tasks-insert.sql
+++ database/tasks-insert.sql
@@ -1,9 +1,41 @@
 DELIMITER ;;
 
 DROP PROCEDURE IF EXISTS `task_insert`;;
-CREATE PROCEDURE `task_insert`(IN insert_name VARCHAR(30), IN insert_description VARCHAR(600), IN insert_start DATE, IN insert_finish DATE, IN insert_status INT, IN insert_active INT)
-BEGIN
-	INSERT INTO `tasks` (`name`, `description`, `start`, `finish`, `status`, `active`) VALUES (insert_name, insert_description, insert_start, insert_finish, insert_status, insert_active);
-END;;
+CREATE PROCEDURE `task_insert`(IN insert_name VARCHAR(30), IN insert_description VARCHAR(600), IN insert_start DATE, IN insert_finish DATE, IN insert_status INT, IN insert_active INT, OUT insert_json VARCHAR(100) )
+main:BEGIN
+	IF DATE(insert_start) = DATE(0) THEN
+		SET insert_json = '{ "success" : false, "code" : 201, "message" : "Invalid start date"  }';
+		LEAVE main;
+	END IF;
 
-DELIMITER ;
+	IF DATE(insert_finish) = DATE(0) THEN
+		SET insert_json = '{ "success" : false, "code" : 202, "message" : "Invalid finish date" }';
+		LEAVE main;
+	END IF;
+
+	IF DATE(insert_finish) < DATE(insert_start) THEN
+		SET insert_json = '{ "success" : false, "code" : 203, "message" : "Finish date is before start date" }';
+		LEAVE main;
+	END IF;
+		
+	INSERT INTO `tasks` (`name`, `description`, `start`, `finish`, `status`, `active`) VALUES (insert_name, insert_description, insert_start, insert_finish, insert_status, insert_active);
+	
+	SET insert_json = '{ "success" : true, 
+	                     "record"  : { 
+	                                  "task_id"      : $task_id$, 
+	                                  "name"         : "$name$", 
+	                                   "description" : "$description$",
+	                                   "start"       : "$start$",
+	                                   "finish"      : "$finish$",	                                    
+	                                   "status"      :  $status$, 
+	                                   "active"      :  $active$ 
+	                                  } 
+	                   }';
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$task_id$',     CAST(LAST_INSERT_ID() AS CHAR));
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$name$',        insert_name);
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$description$', insert_description);
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$start$',       insert_start);
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$finish$',      insert_finish);
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$status$',      insert_status);
+	SET insert_json = REPLACE(insert_json COLLATE utf8_general_ci, '$active$',      insert_active);
+END;;
